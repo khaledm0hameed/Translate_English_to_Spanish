31c711a6506ff960e2da1228b5a81e47edf485c8
admin <khmo492007@gmail.com>
2023-06-14 22:40:16 +0300

    add all src

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ def translate_spanish_to_english(text):
     return translation.text
 
 # Example usage
-spanish_text = input("Enter u word")
+spanish_text = input("Enter u word ::  ")
 english_text = translate_spanish_to_english(spanish_text)
 print(english_text)
