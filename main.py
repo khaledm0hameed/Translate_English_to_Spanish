c650a8497847bde4ae1911d35bc8f61f18ff6721
admin <khmo492007@gmail.com>
2023-06-14 22:36:29 +0300

    add code

---
+++ main.py
@@ -0,0 +1,11 @@
+from googletrans import Translator
+
+def translate_spanish_to_english(text):
+    translator = Translator()
+    translation = translator.translate(text, src='es', dest='en')
+    return translation.text
+
+# Example usage
+spanish_text = input("Enter u word")
+english_text = translate_spanish_to_english(spanish_text)
+print(english_text)
